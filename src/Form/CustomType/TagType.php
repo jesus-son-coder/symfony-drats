602dd3e1a4436e1f91e976d7e0e8cb205df9ccf1
jesus-son-coder <rvck2007@gmail.com>
2019-05-03 09:31:33 +0200

    Form Field Many To Many (Tag)

---
+++ src/Form/CustomType/TagType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form\CustomType;
+
+use App\Entity\Tag;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TagType extends AbstractType
+{
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    /*
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Tag::class,
+        ]);
+    }
+    */
+}
