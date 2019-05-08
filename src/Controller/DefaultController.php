76df9ec623dd0d3963c262ec39e38a5de6d60218
jesus-son-coder <rvck2007@gmail.com>
2019-05-08 21:03:32 +0200

    Ajouter une Transaction SQL pour sécuriser la persistence des objets. Si une instruction arrivait à planter entre le début de la transaction et le commit de cette transaction, alors Doctrine effectuera un Rollback pour éviter toute persistence !

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -20,11 +20,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
+
 
     /**
      * @param Request $request
@@ -46,12 +48,24 @@ class DefaultController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()) {
 
+            $em = $this->getDoctrine()->getManager();
+            $this->getDoctrine()->getConnection();
+
+            /* La transaction permettra d'invalider la peristence de $comment
+                dès qu'une instruction comprise entre le "beginTransaction" et le "em->commit" buggue !
+
+                Si une instruction entre ces deux instructions plante, alors Doctrine effectue un Rollback !
+            */
+            $em->getConnection()->beginTransaction();
+
             $manager->persist($comment);
             $manager->flush();
 
             /* Générer l'évenement et le propager dans l'application : */
             $event = new CommentPublishedEvent($comment);
             $dispatcher->dispatch(CommentPublishedEvent::NAME, $event);
+
+            $em->commit();
 
         }
 
