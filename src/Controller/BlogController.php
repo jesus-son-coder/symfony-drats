a403a562d0d568d7f4b6ea8c896d15ea3f2ecca5
jesus-son-coder <rvck2007@gmail.com>
2019-05-10 08:51:04 +0200

    Utilisation d'un petit service "Greeting" dans le contrôleur

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sekaherve
+ * Date: 10/05/2019
+ * Time: 01:00
+ */
+
+namespace App\Controller;
+
+use App\Service\Greeting;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends Controller
+{
+    private $greeting;
+
+    private $badDesign;
+
+    public function __construct(Greeting $greeting)
+    {
+        $this->greeting = $greeting;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @Route("/home", name="blog_index")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request)
+    {
+
+        $service_greetinng = $this->get('app.greeting');
+
+        return $this->render('base.html.twig', ['message' => $service_greetinng->hello()]);
+    }
+}
---
+++ src/Service/Greeting.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sekaherve
+ * Date: 10/05/2019
+ * Time: 01:14
+ */
+
+namespace App\Service;
+
+
+class Greeting
+{
+    public function hello()
+    {
+        return 'Hello my friend ';
+    }
+}
