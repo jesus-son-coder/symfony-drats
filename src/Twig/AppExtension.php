38be07793c264ff37ab9390f580c3500eda72b23
jesus-son-coder <rvck2007@gmail.com>
2019-05-10 23:19:23 +0200

    Global variables, from Service to Twig/view
    [variable globale]

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -10,14 +10,29 @@ namespace App\Twig;
 
 
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
+    private $locale;
+
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
+
     public function getFilters()
     {
         return [
             new TwigFilter('price', [$this, 'priceFilter'])
+        ];
+    }
+
+    public function getGlobals()
+    {
+        return [
+          'locale' => $this->locale
         ];
     }
 
