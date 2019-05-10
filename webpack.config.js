ce12853b45963e655bf715473d849c243257494c
jesus-son-coder <rvck2007@gmail.com>
2019-05-11 01:39:57 +0200

    -------------------------------------- WEBPACK FOR ASSET BUNDLING - Partie 2 -------------------------------------- Source du Tuto : https://www.udemy.com/learn-symfony-4-hands-on-creating-a-real-world-application/learn/v4/t/lecture/10197568?start=0
    Install Bootstrap via Yarn package manager :
    yarn add bootstrap@4.0.0
    
    Install the JavaScript Library called "holderjs" for displaying the image placeholder :
    yarn add holderjs
    
    Install Popper.js :
    yarn add popper.js@1.12.9
    
    Install JQuery :
    yarn add jquery@3.3.1
    
    Après ces dernières installations, il faut builder Encore :
    Sous Windows :
    node_modules\.bin\encore dev
    
    Sous Linux :
    ./node_modules\.bin\encore dev
    
    Exécution automatique du build Encore :
    ---------------------------------------
    Il faut rajouter le "Watcher" en option.
    Exemple :
    node_modules\.bin\encore dev --watch
    
    To stop the Watch process, type "CTRL + C"

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,25 @@ Encore
 
     // The public path used by the web server to access the previous directory :
     .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
 
     // Define the assets of the project :
-    .addEntry('js/app', ['./assets/js/app.js'])
-    .addStyleEntry('css/app', ['./assets/css/app.scss'])
+    .addEntry('js/app', [
+        './node_modules/jquery/dist/jquery.slim.js',
+        './node_modules/popper.js/dist/popper.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/holderjs/holder.min.js'
+    ])
+    .addStyleEntry('css/app', [
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './assets/css/app.css'
+    ])
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
 ;
 
 module.exports = Encore.getWebpackConfig();
