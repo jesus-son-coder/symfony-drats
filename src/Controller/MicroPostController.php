624585ea491279f64b19b291d8b046f8c90594b4
jesus-son-coder <rvck2007@gmail.com>
2019-05-11 14:06:47 +0200

    Filtrer sur les plus récent, grace à la méthode "findBy"

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -41,7 +41,7 @@ class MicroPostController extends AbstractController
     public function index()
     {
         return $this->render('micro-post/index.html.twig',[
-            'posts' => $this->microPostRepository->findAll()
+            'posts' => $this->microPostRepository->findBy([], ['time' => 'DESC'])
         ]);
     }
 
