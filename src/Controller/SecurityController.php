027362200f4c0c2b5fa2e0ff3fce955c7691783e
jesus-son-coder <rvck2007@gmail.com>
2019-05-13 01:26:09 +0200

    Création du controller "SecurityController" qui va gérer la connexion (function "login()") et la déconnexion (function "logout()")

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sekaherve
+ * Date: 13/05/2019
+ * Time: 01:08
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        return $this->render('security/login.html.twig', [
+            'error' => $utils->getLastAuthenticationError(),
+            'last_username' => $utils->getLastUsername()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
+
+    }
+}
