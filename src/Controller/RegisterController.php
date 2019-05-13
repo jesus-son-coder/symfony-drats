ffb6b067dcdb24a351a9f00270b62732b653c7dd
jesus-son-coder <rvck2007@gmail.com>
2019-05-13 14:10:06 +0200

    User Registration Controller et Template "register.html.twig" https://www.udemy.com/learn-symfony-4-hands-on-creating-a-real-world-application/learn/v4/t/lecture/10198240?start=0

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sekaherve
+ * Date: 13/05/2019
+ * Time: 13:31
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword(
+                $user,
+                $user->getPlainPassword()
+            );
+            $user->setPassword($password);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('micro_post_index');
+        }
+
+        return $this->render('register/register.html.twig', [
+                'form' => $form->createView()
+            ]
+        );
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,7 +8,6 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
