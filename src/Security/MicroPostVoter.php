573b16c4e17b8b96993c7bb8d9d2cbf3b6f4aed1
jesus-son-coder <rvck2007@gmail.com>
2019-05-14 21:44:38 +0200

    Restricting Access to "edit" or "delete" a MicroPost using "Security Voters" : https://www.udemy.com/learn-symfony-4-hands-on-creating-a-real-world-application/learn/v4/t/lecture/10198290?start=0
    La méthode "supports($attribute, $subject)" permet de déterminer si notre Voter (ici "MicroPostVoter") est habilité à décider de vérifier une autorisation, en se basant sur l'attribut et le Sujet fournis en paramètre.
    
    L'attribut pourrait être "Edit", ou "Delete", ou "Create", ...
    Le "sujet" pourrait être une instance de l'objet "MicroPost".
    
    Ainsi, cette méthode "supports()" retournera un boolean (true or false) pour répondre à la question de savoir si le Voter actuel a le droit de VERIFIER l'autorisation ou la restriction de l'accès aux actions contenues dans la variable $attribute ("delete", "edit"...) pour certains objets/sujets ("MicroPost", etc...).
    Il s'git juste d'une Pre-vérification. En effet, cette vérification ne s'effectue pas encore dans la méthode "supports()" mais elle se fera plutôt dans la méthode "voterOnAttribute()" qui vient juste après.
    
    La méthode "VoterOnAttribute($attribute, $subject, $token)" permet de vérifier réellement les droits d'effectuer les actions souhaitées. Cette méthode contient la logique métier de vérification.
    Le paramètre "$token" en argument représente l'Utilisateur actuellement connecté. Il retourne donc une instance de l'entité User.

---
+++ src/Security/MicroPostVoter.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sekaherve
+ * Date: 14/05/2019
+ * Time: 09:04
+ */
+
+namespace App\Security;
+
+use App\Entity\MicroPost;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class MicroPostVoter extends Voter
+{
+    const EDIT = 'edit';
+    const DELETE = 'delete';
+
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute. Il could be "Edit" or "Delete" or "Create"
+     * @param mixed $subject The subject to secure, e.g. an object (like "MicroPost") the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject)
+    {
+        if(!in_array($attribute, [self::EDIT, self::DELETE])) {
+            return false;
+        }
+
+        if(!$subject instanceof MicroPost) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $authenticatedUser = $token->getUser();
+
+        /* Nous ne souhaitons pas qu'unn utlisateur non connecté puisse effectuer les actions
+            mentionnés dans le paramètre "$attributes" (à savoir : "EDIT" ou "DELETE") */
+        if(!$authenticatedUser instanceof User) {
+            return false;
+        }
+        /**
+         * @var MicroPost $microPost
+         */
+        $microPost = $subject;
+
+        /* Nous allons ensuite vérifier que l'Utilisateur ayant publié le MicorPost est le meême que l'Utilisateur connecté. Car la règle serait que : seuls les utilsiateurs ayant publié des articles ont le droit d'éditer ou de supprimer que leurs propres articles.
+        Cette condition est la seule suffisante pour accorder les droits de modification ou de suppression à l'utilisateur qui en fait la demande. Voilà pourquoi nous effectuons un 'return" à ce niveau :
+         */
+        return ($microPost->getUser()->getId() === $authenticatedUser->getId());
+
+    }
+
+}
