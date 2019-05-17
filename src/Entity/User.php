4890eed2f7220d1744cd3924be2de54d70f99cee
jesus-son-coder <rvck2007@gmail.com>
2019-05-18 00:50:14 +0200

    ManyToMany Self-referencing Relation (Following/Followers) https://www.udemy.com/learn-symfony-4-hands-on-creating-a-real-world-application/learn/v4/t/lecture/10198828?start=0
    Permettre à un User de suivre un autre User.
    Un User peut lui-même être un Follower, et il peut avoir des Followers (qui sont d'autres USrs).
    C'est un bon exemple pour les Relations de type ManyToMany Self-Referencing.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,6 +64,25 @@ class User implements UserInterface, \Serializable
     private $posts;
 
     /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", mappedBy="following")
+     */
+    private $followers;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="followers")
+     *
+     * @ORM\JoinTable(name="following",
+     *     joinColumns={
+     *          @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     *     },
+     *     inverseJoinColumns={
+     *          @ORM\JoinColumn(name="following_user_id", referencedColumnName="id")
+     *     }
+     * )
+     */
+    private $following;
+
+    /**
      * @ORM\Column(type="string", length=50)
      * @Assert\NotBlank()
      * @Assert\Length(min=4, max=50)
@@ -73,6 +92,8 @@ class User implements UserInterface, \Serializable
     public function __construct()
     {
         $this->posts = new ArrayCollection();
+        $this->followers = new ArrayCollection();
+        $this->following = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -236,6 +257,24 @@ class User implements UserInterface, \Serializable
         $this->plainPassword = $plainPassword;
     }
 
-	/* test */
+    /**
+     * @return mixed
+     */
+    public function getFollowers()
+    {
+        return $this->followers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFollowing()
+    {
+        return $this->following;
+    }
+
+
+
+
 
 }
